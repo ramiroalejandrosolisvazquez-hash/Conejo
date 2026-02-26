4189bba1432b7c21332f5fb1b303aea31da5ef78
ramiroalejandrosolisvazquez-hash <ramiroalejandrosolisvazquez@gmail.com>
2026-02-26 11:22:29 -0700

    Add files via upload

---
+++ Articulo.py
@@ -0,0 +1,5 @@
+class Articulo:
+    def __init__(self, fecha: str , cantidad: list , total : str):
+        self. fecha = fecha
+        self. cantidad = cantidad
+        self. total = total
---
+++ Venta.py
@@ -0,0 +1,9 @@
+
+from calcular_total import Calcular_total
+
+class Venta:
+
+ def __init__(self, articulos: str, calcular_total: list, venta: str):
+    self.articulos = articulos
+    self.calcular_total = calcular_total
+    self.venta = venta
